0481e97cbc051f660dbff794a7371f8e589729e6
jonatsts <jonatassantos96@hotmail.com>
2019-08-13 22:43:29 -0300

    add vs2 do game

--- cacaPalavras_vs2/src/cacaPalavras/Servidor.java
+++ cacaPalavras_vs2/src/cacaPalavras/Servidor.java
@@ -1,17 +1,17 @@
-package negocio;
+package cacaPalavras;
 
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Servidor {
 	public static void main(String[] args) {
 		try {
 			ServerSocket server = new ServerSocket(1234);
 			System.out.println("Servidor em rede");
 			while (true) {
 				Socket cliente = server.accept();
-                                System.out.println("Cliente [" + cliente.getInetAddress() + "] conectado"  );
-				Thread thread = new Thread(new AdvinhaPalavra(cliente));
+                                System.out.println("Cliente IP: [" + cliente.getInetAddress().toString().split("/")[1] + "] Porta: " + cliente.getLocalPort() + " - conectado"  );
+				Thread thread = new Thread(new CacaPalavra(cliente));
 				thread.start();
 			}
 			
