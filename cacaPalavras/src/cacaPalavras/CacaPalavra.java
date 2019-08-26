f1662693074fc8c6936f8f1e07d2e195e7c8bec0
jonatsts <jonatassantos96@hotmail.com>
2019-08-26 16:35:02 -0300

    atualização do jogo Vs 5

--- cacaPalavras/src/cacaPalavras/CacaPalavra.java
+++ cacaPalavras/src/cacaPalavras/CacaPalavra.java
@@ -6,24 +6,18 @@ import java.net.Socket;
 import java.util.Random;
 import java.util.Scanner;
 
-//  ♠Implementações Pendentes♠
-//    • Conometrar 2 min para acertar todas as palavras
-//    • Pontuar os acertos com base na quantidade de letras
-//      -> 4 letras = 1 ponto;
-//      -> 5 letras = 3 pontos;
-//      -> 6 ou mais letras = 5 pontos!
-//    • Exibir regras ao iniciar jogo
-//    • Perguntar e exbir para o usuario o tema do caça palavra
-//    • receber feedback dos usuarios sobre o jogo ou possivel melhoria (OPCIONAL)
-
 public class CacaPalavra implements Runnable {
-    Quadros quadros = new Quadros();
+
     Socket cliente;
-    String[] palavras = quadros.getQuadroPalavras().get("esporte").getPalavras();
+    String[] palavras = {"casa", "barco", "samba", "corda"};
     String[] palavrasUsuario = new String[palavras.length];
-    String quadroPalavras = quadros.getQuadroPalavras().get("esporte").getQuadro();
+    String quadroPalavras =   "J A S C O R D A X Y \n"
+                            + "C A N A Ú G V L Á R \n"
+                            + "A H B Á M U I G Í G \n"
+                            + "S Ç Ú U N B Q A E I \n"
+                            + "A A E L T B A R C O \n";
+
     String palavraUsuario;
-    
     //int posicaoSorteada;
     int acertos;
     int qtdPalavras = palavras.length;
@@ -31,14 +25,12 @@ public class CacaPalavra implements Runnable {
 
     public CacaPalavra(Socket cliente) {
         this.cliente = cliente;
-        int numAleatorio = new Random().nextInt(4);
+        Random numAleatorio = new Random();
         acertos = 0;
         tentativas = 0;
-        inicializarPalavrasUsuario();
     }
 
     @Override
-    @SuppressWarnings("CallToPrintStackTrace")
     public void run() {
         try {
             Scanner in = new Scanner(this.cliente.getInputStream());
@@ -51,7 +43,7 @@ public class CacaPalavra implements Runnable {
 
             while (acertos <= qtdPalavras) {
                 palavraUsuario = in.next();
-                int acertou = verificaPalavra(palavraUsuario);
+                Boolean acertou = verificaPalavra(palavraUsuario);
                 
                 if (acertos == 4) {
                     tentativas++;
@@ -63,52 +55,35 @@ public class CacaPalavra implements Runnable {
                     break;
                 }
 
-                switch (acertou) {
-                    case 1:
-                        tentativas++;
-                        out.print("nao\n");
-                        out.println("Voce achou: \"" + palavraUsuario + "\" ☻");
-                        break;
-                    case -1:
-                        tentativas++;
-                        out.print("nao\n");
-                        out.println("Palavra não encontrada !");
-                        break;
-                    default:
-                        tentativas++;
-                        out.print("nao\n");
-                        out.println("Palavra já digitada !");
-                        break;
+                if (acertou) {
+                    tentativas++;
+                    out.print("nao\n");
+                    out.println("Voce achou: \"" + palavraUsuario + "\" ☻");
+                } else if (!acertou) {
+                    tentativas++;
+                    out.print("nao\n");
+                    out.println("Palavra não encontrada !");
                 }
+
             }
 
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    public int verificaPalavra(String palavra) {
-        int i=0;
-        for (String palavraDigitada : palavrasUsuario) {
-            if(palavra.equalsIgnoreCase(palavraDigitada))
-                return 0;
-            
-            for (String palavraRegistrada : palavras) {
-                if (palavra.equalsIgnoreCase(palavraRegistrada)) {
-                    palavrasUsuario[i] = palavra;
-                    acertos++;
-                    i++;
-                    return 1;
-                }
+    public boolean verificaPalavra(String palavraUsuario) {
+        boolean acertou = false;
+
+        for (String palavra : palavras) {
+            if (palavraUsuario.equalsIgnoreCase(palavra)) {
+                acertou = true;
+                acertos++;
             }
         }
-        return -1;
-    }
 
-    private void inicializarPalavrasUsuario() {
-        for (int i = 0; i < palavrasUsuario.length; i++) {
-            palavrasUsuario[i] = "";
-        }
+        return acertou;
     }
 
 }
