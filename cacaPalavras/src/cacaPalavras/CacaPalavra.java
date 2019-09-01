4ab90591aea6ee8482b1b784d83e71ae2062e22d
jonatsts <jonatassantos96@hotmail.com>
2019-08-31 22:51:29 -0300

    add nova versão do game

--- cacaPalavras/src/cacaPalavras/CacaPalavra.java
+++ cacaPalavras/src/cacaPalavras/CacaPalavra.java
@@ -3,38 +3,39 @@ package cacaPalavras;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Random;
 import java.util.Scanner;
 
 //  ♠Implementações Pendentes♠
-//    • Conometrar 2 min para acertar todas as palavras
-//    • Pontuar os acertos com base na quantidade de letras
-//      -> 4 letras = 1 ponto;
-//      -> 5 letras = 3 pontos;
-//      -> 6 ou mais letras = 5 pontos!
-//    • Exibir regras ao iniciar jogo
-//    • Perguntar e exbir para o usuario o tema do caça palavra
-//    • receber feedback dos usuarios sobre o jogo ou possivel melhoria (OPCIONAL)
+//    • Mostrar a contagem regressiva para o usuario
+//    • Mostra quantidade de palavras o usuario já acertou - OPCIONAL
 
 public class CacaPalavra implements Runnable {
-    Quadros quadros = new Quadros();
-    Socket cliente;
-    String[] palavras = quadros.getQuadroPalavras().get("esporte").getPalavras();
-    String[] palavrasUsuario = new String[palavras.length];
-    String quadroPalavras = quadros.getQuadroPalavras().get("esporte").getQuadro();
-    String palavraUsuario;
+    private Thread contador;
+    private Quadros quadros;
+    private Socket cliente;
     
-    //int posicaoSorteada;
+    private String[] temas = {"aleatorio", "esporte", "musica"};
+    private String[] palavras;// = quadros.getQuadroPalavras().get("esporte").getPalavras();
+    private String[] palavrasUsuario;// = new String[palavras.length];
+    private String quadroPalavras;// = quadros.getQuadroPalavras().get("esporte").getQuadro();
+    private String palavraUsuario;
+    private String tempoTotal;
+
+    private short time;
+    private short tempoLimite = 2;
+    
+    int qtdPalavras;
     int acertos;
-    int qtdPalavras = palavras.length;
     int tentativas;
+    int pontuacao;
 
     public CacaPalavra(Socket cliente) {
         this.cliente = cliente;
-        int numAleatorio = new Random().nextInt(4);
+        quadros = new Quadros();
         acertos = 0;
         tentativas = 0;
-        inicializarPalavrasUsuario();
+        pontuacao = 0;
+        //int numAleatorio = new Random().nextInt(4);
     }
 
     @Override
@@ -43,43 +44,84 @@ public class CacaPalavra implements Runnable {
         try {
             Scanner in = new Scanner(this.cliente.getInputStream());
             PrintWriter out = new PrintWriter(this.cliente.getOutputStream(), true);
-            out.println("CAÇA PALAVRAS\n");
-            out.flush();
-
-            out.print(quadroPalavras);
-            out.flush();
-
-            while (acertos <= qtdPalavras) {
-                palavraUsuario = in.next();
-                int acertou = verificaPalavra(palavraUsuario);
+            
+            inicio:
+            while(true){
+            
+                mostrarRegras(in, out);
+                escolheQuadro(in, out);
                 
-                if (acertos == 4) {
-                    tentativas++;
-                    out.print("fim\n");
-                    out.println("Voce achou: \"" + palavraUsuario + "\" ☻");
-                    out.println("Total de tentativas: " + tentativas);
-                    out.println("Parabéns !! Você encontrou todas as palavras ☻\n");
-                    cliente.close();
-                    break;
-                }
+                //começa a contar o tempo
+                contador = new Thread(conometro);
+                contador.start();
 
-                switch (acertou) {
-                    case 1:
+                out.println("\nCAÇA PALAVRAS\n");
+                out.flush();
+
+                out.print(quadroPalavras);
+                out.flush();
+
+                while (acertos <= qtdPalavras) {
+                    palavraUsuario = in.next();
+
+                    if(palavraUsuario.equals("fim")){
+                        try {
+                            cliente.close();
+                            contador.stop();
+                            Thread.currentThread().stop();
+                            break;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int acertou = verificaPalavra(palavraUsuario);
+                    
+                    if(tempoLimite == 0){
+                        out.print("acabou\n");
+                        out.print("Tempo Esgotado -> [0"+tempoTotal+"0]\n");
+                        contador.stop();
+                        acertos = 0;
+                        tempoLimite = 2;
+                        tentativas = 0;
+                        continue inicio;
+                    }
+
+                    if (acertos == 5) {
                         tentativas++;
-                        out.print("nao\n");
+                        out.print("fim\n");
                         out.println("Voce achou: \"" + palavraUsuario + "\" ☻");
-                        break;
-                    case -1:
-                        tentativas++;
-                        out.print("nao\n");
-                        out.println("Palavra não encontrada !");
-                        break;
-                    default:
-                        tentativas++;
-                        out.print("nao\n");
-                        out.println("Palavra já digitada !");
-                        break;
-                }
+                        out.println("Parabéns !! Você encontrou todas as palavras ☻");
+                        out.println("Total de tentativas: " + tentativas);
+                        out.println("Pontuação: " + pontuacao + "\n");
+                        out.println("Tempo total: " + tempoTotal);
+                        
+                        contador.stop();
+                        acertos = 0;
+                        tempoLimite = 2;
+                        tentativas = 0;
+                        continue inicio;
+                    } 
+                    else{
+                        switch (acertou) {
+                            case 1:
+                                tentativas++;
+                                out.print("nao\n");
+                                out.println("Voce achou: \"" + palavraUsuario + "\" ☻");
+                                break;
+                            case -1:
+                                tentativas++;
+                                out.print("nao\n");
+                                out.println("Palavra não encontrada !");
+                                break;
+                            default:
+                                tentativas++;
+                                out.print("nao\n");
+                                out.println("Palavra já digitada !");
+                                break;
+                        }
+                    }
+                }// fim do primeiro while
             }
 
         } catch (IOException e) {
@@ -87,6 +129,34 @@ public class CacaPalavra implements Runnable {
         }
     }
 
+    private Runnable conometro = new Runnable() {
+        @Override
+        public void run() {
+            try{
+                    short minutos = 1 ;
+                    short segundos = 59;
+                    
+                    while(true){
+                        if(minutos == 0 && segundos == 0){
+                            String format = minutos + ":"+ segundos;
+                            tempoTotal = format;
+                            tempoLimite = minutos;
+                            break;
+                        }
+                            
+                        if(segundos == 0){
+                            segundos = 59;
+                            minutos--;
+                        }
+                        Thread.sleep(1000);
+                        segundos--;
+                    }
+                
+            } catch (Exception e){}
+ 
+        }
+    };
+    
     public int verificaPalavra(String palavra) {
         int i=0;
         for (String palavraDigitada : palavrasUsuario) {
@@ -95,6 +165,7 @@ public class CacaPalavra implements Runnable {
             
             for (String palavraRegistrada : palavras) {
                 if (palavra.equalsIgnoreCase(palavraRegistrada)) {
+                    calculaPontuacao(palavra.length());
                     palavrasUsuario[i] = palavra;
                     acertos++;
                     i++;
@@ -109,6 +180,82 @@ public class CacaPalavra implements Runnable {
         for (int i = 0; i < palavrasUsuario.length; i++) {
             palavrasUsuario[i] = "";
         }
+    }
+
+    private void calculaPontuacao(int tamanhoPalavra) {
+
+        switch (tamanhoPalavra) {
+            case 4:
+                pontuacao+=1;
+                break;
+            case 5:
+                pontuacao+=3;
+                break;
+            default:
+                pontuacao+=5;
+                break;
+        }
+    }
+    
+    @SuppressWarnings("CallToPrintStackTrace")
+    private void escolheQuadro(Scanner in, PrintWriter out) {
+        String temaUsuario;
+        boolean temaValido = false;
+        
+        out.println("Escolha o tema do Caça Palavras ou digite \"SAIR\" para encerrar o jogo");
+        out.println("Aleatorio | Esporte | Musica\n");
+        temaUsuario = in.next();
+        temaUsuario = temaUsuario.toLowerCase();
+        
+        if(temaUsuario.equals("fim")){
+            try {
+                cliente.close();
+                Thread.currentThread().stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } 
+        else{
+            for (String tema : temas) {
+                if(temaUsuario.equals(tema)){
+                    temaValido = true;
+                }
+            }
+            if(temaValido == false)
+                temaUsuario = "aleatorio";
+               
+            palavras = quadros.getQuadroPalavras().get(temaUsuario).getPalavras();
+            palavrasUsuario = new String[palavras.length];
+            qtdPalavras = palavras.length;
+            quadroPalavras = quadros.getQuadroPalavras().get(temaUsuario).getQuadro();
+            inicializarPalavrasUsuario();
+                
+                
+//            if(!temaValido){
+//                palavras = quadros.getQuadroPalavras().get("aleatorio").getPalavras();
+//                palavrasUsuario = new String[palavras.length];
+//                qtdPalavras = palavras.length;
+//                quadroPalavras = quadros.getQuadroPalavras().get("aleatorio").getQuadro();
+//                inicializarPalavrasUsuario();
+//            }
+        }
+    }
+    
+    private void mostrarRegras(Scanner in, PrintWriter out) {
+        out.println("#################################################################################################");
+        out.println("##                                       REGRAS DO JOGO                                        ##");
+        out.println("## -> As palavras têm que ter pelo menos 3 letras                                              ##");
+        out.println("## -> Forme palavras utilizando letras que estejam juntas nas direções: Vertical ou horizontal ##");
+        out.println("## -> Desconsidere acentos ao formar palavras. (Um “C” pode ser “Ç” ou um “A” pode ser “Ã”)    ##");
+        out.println("## -> Forme qualquer palavra no plural ou singular                                             ##");
+        out.println("## -> Não valem nomes próprios ou palavras estrangeiras                                        ##");
+        out.println("## -> Tempo para encontrar as palavras: 2 Minutos                                                                                            ##");
+        out.println("##                                                                                             ##");
+        out.println("## -> PONTUAÇÃO:  4 letras = 1 ponto | 5 letras = 3 pontos | 6 ou mais letras = 5 pontos       ##");
+        out.println("##                                                                                             ##");
+        out.println("## -> Para encerrar o jogo digite \"SAIR\"                                                       ##");
+        out.println("#################################################################################################\n");
+        
     }
 
 }
--- cacaPalavras/src/cacaPalavras/Cliente.java
+++ cacaPalavras/src/cacaPalavras/Cliente.java
@@ -1,11 +1,15 @@
 package cacaPalavras;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Cliente {
 
+    @SuppressWarnings({"CallToPrintStackTrace", "UseSpecificCatch"})
     public static void main(String[] args) {
         try {
             Socket client = new Socket("", 1234);
@@ -13,37 +17,144 @@ public class Cliente {
             Scanner inUser = new Scanner(System.in);
             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
             String saida = "";
+            String palavraDigitada;
+            boolean continua;
+            
+            inicioGame:
+            while(true) {
+                //Exibe as regras do jogo
+                exibirRegras(in);
 
-            // Imprime o nome: CAÇA PALAVRAS
-            System.out.println(in.nextLine());
+                //Escolhe o tema do jogo
+                continua = escolherTema(in, out, inUser, client);
 
-            //Imprime o quadro de palavras
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
+                if(continua){
 
-            while (true) {
-                if( saida.equalsIgnoreCase("fim")){
-                    System.out.println(in.nextLine());
-                    System.out.println();
-                    System.out.println(in.nextLine());
-                    System.out.println();
-                    System.out.println(in.nextLine());
+                    //Exibe o nome "CAÇA PALAVRAS e o Quadro de palavras"
+                    exibeQuadro(in);
+                    
+                    
+                    while (true) {
+                        //MUDAR A ORDEM DAS CONDICIONAIS
+             
+                        if(saida.equalsIgnoreCase("fim")){
+                            System.out.println(in.nextLine());
+                            System.out.println();
+                            System.out.println(in.nextLine());
+                            System.out.println();
+                            System.out.println(in.nextLine());
+                            System.out.println(in.nextLine());
+                            System.out.println(in.nextLine());
+                            saida = "";
+                            newGame();
+                            continue inicioGame;
+                        }
+                        
+                        if(saida.equalsIgnoreCase("acabou")){
+                            System.out.println(in.nextLine());
+                            saida = "";
+                            newGame();
+                            continue inicioGame;
+                        }
+
+                        System.out.println("\nEscreva uma Palavra ou \"SAIR\" para encerrar o jogo");
+                        palavraDigitada = inUser.nextLine();
+                        if(palavraDigitada.equalsIgnoreCase("sair")){
+                            endGame(out,client);
+                            break inicioGame;
+                        }
+
+                        out.println(palavraDigitada);
+                        System.out.println();
+                        saida = in.nextLine();
+                        
+                        if(saida.equalsIgnoreCase("acabou")){
+                            System.out.println(in.nextLine());
+                            saida = "";
+                            newGame();
+                            continue inicioGame;
+                        }
+                        System.out.println(in.nextLine());
+                    }// Fim do Jogo
+                } //Executa se continua for true
+                else{ // se continuar for false
                     break;
                 }
-                System.out.println("\nEscreva uma Palavra: ");
-                out.println(inUser.nextLine());
-                System.out.println();
-                saida = in.nextLine();
-                System.out.println(in.nextLine());
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
+    private static void exibirRegras(Scanner in) {
+        for(int i=0; i<14; i++)
+            System.out.println(in.nextLine());
+    }
+    
+    private static boolean escolherTema(Scanner in, PrintWriter out, Scanner inUser, Socket client) {
+        String tema;
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+        tema = inUser.nextLine();
+        
+        try{
+            if(tema.equalsIgnoreCase("sair")){
+                endGame(out,client);
+                return false;
+            } else
+                out.println(tema); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+    
+    private static void exibeQuadro(Scanner in) {
+        // Exibe o nome: CAÇA PALAVRAS
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+
+        //Exibe o quadro de palavras
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+        System.out.println(in.nextLine());
+    }
+    
+    private static void newGame(){
+        try{
+            Thread.sleep(5000);
+            limparConsole();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void endGame(PrintWriter out, Socket client){
+        try{
+            limparConsole();
+            out.println("fim");
+            System.out.println("FIM DE JOGO !!");
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void limparConsole() {
+        try {
+            Robot robot = new Robot();
+            robot.setAutoDelay(10);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_L);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_L);
+        } catch (AWTException ex) {
+        }
+    }
+    
 }
